83a8b4b9c645e894fc675a7b36d00b8fcb6f38ba
TOMOYAITO0726 <tomoyaito199507265724@gmail.com>
2023-01-08 12:30:00 +0000

    [chapter4]

---
+++ chapter4.java
@@ -0,0 +1,13 @@
+public class chapter4{
+  public static void main(String[] args){
+    
+    String str1 = "TOMOYA ITO";
+    System.out.println(str1);
+    
+    str1 = "チャレンジ";
+    System.out.println(str1); 
+    
+    final int num1 = 5;
+    System.out.println(num1);
+  }
+}
