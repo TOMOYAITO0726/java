485afda129913a8139a729b8edb325eb5e01229f
TOMOYAITO0726 <tomoyaito199507265724@gmail.com>
2023-01-09 11:08:51 +0000

    [型の基本]

---
+++ chapter05.java
@@ -0,0 +1,18 @@
+public class chapter05{
+  public static void main(String[] args) {
+    long long1 = 545454545454545L;
+    System.out.println(long1);
+    double double1 = 5.4f;
+    System.out.println(double1);
+    boolean boolean1 = true;
+    System.out.println(boolean1);
+    Float Float1 = 4.2f;
+    System.out.println(Float1);
+    Integer Integer1 = 100;
+    System.out.println(Integer1);
+    Character  Character1 = 'う';
+    System.out.println(Character1);
+    int [] array = {0, 10, 20, 30, 40};
+    System.out.println(array[3]);
+ }
+}
---
+++ chapter6.java
@@ -0,0 +1,8 @@
+public class chapter6{
+  public static void main(String[] args) {
+    int num1 = 15;
+    int num2 =  3;
+    int ans = num1 + num2;
+System.out.println (ans);  
+  }
+}
