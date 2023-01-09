a95fa66658265a68789e229550729f75fce18a33
TOMOYAITO0726 <tomoyaito199507265724@gmail.com>
2023-01-09 12:36:33 +0000

    [インクリメント・デクリメント]

--- chapter6.java
+++ chapter6.java
@@ -4,5 +4,13 @@ public class chapter6{
     int num2 =  3;
     int ans = num1 + num2;
 System.out.println (ans);  
+
+    int num4 = 100;
+    num4++;
+System.out.println (num4);/*インクリメント*/
+    num4--;
+System.out.println (num4);/*デクリメント*/   
+
+
   }
 }
