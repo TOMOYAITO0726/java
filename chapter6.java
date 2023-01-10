a06840d3ebf9ee5433e622f94acb9ff541a72f7e
TOMOYAITO0726 <tomoyaito199507265724@gmail.com>
2023-01-10 10:08:51 +0000

    [演算子]

--- chapter6.java
+++ chapter6.java
@@ -10,7 +10,20 @@ System.out.println (ans);
 System.out.println (num4);/*インクリメント*/
     num4--;
 System.out.println (num4);/*デクリメント*/   
+     /*確認問題*/
+     int val = 2 + 5;  
+System.out.println(val);
+     val++;
+System.out.println(val);     
+     
+     val *= 5;
+System.out.println(val);     
 
+    boolean bool = (val == 50);
+System.out.println(bool);
 
+    bool =(val<50)&&(val%10==0);
+System.out.println(bool);    
+    
   }
 }
