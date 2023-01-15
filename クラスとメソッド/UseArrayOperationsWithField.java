ea941b98b91cb81d195f902fb421fbb044f4b1fd
TOMOYAITO0726 <tomoyaito199507265724@gmail.com>
2023-01-15 06:12:38 +0000

    [クラス]

---
+++ クラスとメソッド/UseArrayOperationsWithField.java
@@ -0,0 +1,10 @@
+public class UseArrayOperationsWithField{
+  public static void main(String[] args){
+    int[] list = {4,5,84,7,1,15};
+    
+    ArrayOperationsWithField arrayOpe = new ArrayOperationsWithField();
+    int sum = arrayOpe.getSumArray(list);
+    double ave = arrayOpe.getAverage();
+    System.out.println("配列内の合計: " + sum + " 平均: " +ave);
+  }
+}
