3c64f0014487c170e43cc1ef8a0d9676b0349b0c
TOMOYAITO0726 <tomoyaito199507265724@gmail.com>
2023-01-29 02:53:04 +0000

    [配列]

---
+++ 配列/WithoutArray.java
@@ -0,0 +1,16 @@
+public class WithoutArray{
+  public static void main(String[] args){
+    int num1 = 8;
+    int num2 = 9;
+    int num3 = 17;
+    int num4 = 26;
+    int num5 = 43;
+    
+    System.out.println("num1:"+num1);
+    System.out.println("num1:"+num2);
+    System.out.println("num1:"+num3);
+    System.out.println("num1:"+num4);
+    System.out.println("num1:"+num5);
+    
+  }
+}
