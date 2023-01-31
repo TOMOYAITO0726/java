f9abf771570a0f9947d281c22fe296760b84dfc4
TOMOYAITO0726 <tomoyaito199507265724@gmail.com>
2023-01-31 13:05:22 +0000

    [配列]

---
+++ 配列/WithArray2.java
@@ -0,0 +1,11 @@
+public class WithArray2{
+  public static void main(String[] args){
+    int[] nums ={8,9,17,26,43};
+    
+    System.out.println("1個目:"+nums[0]);
+    System.out.println("2個目:"+nums[1]);
+    System.out.println("3個目:"+nums[2]);
+    System.out.println("4個目:"+nums[3]);
+    System.out.println("5個目:"+nums[4]);
+  }
+}
