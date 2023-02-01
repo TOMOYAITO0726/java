b3b197771bebb9ac226e727b1cf3aad53f220d32
TOMOYAITO0726 <tomoyaito199507265724@gmail.com>
2023-02-01 12:08:43 +0000

    [配列とループ]

---
+++ 配列/ArrayLoop.java
@@ -0,0 +1,9 @@
+public class ArrayLoop{
+  public static void main(String[] args){
+    int[] nums ={8,9,17,26,43};
+    
+    for(int i=0; i<=4; i++){
+      System.out.println((i+1)+"個目:"+nums[i]);
+    }
+  }
+}
