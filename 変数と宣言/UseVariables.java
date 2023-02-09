c1a7bb2200dc12e4a62597c74680da91325fb3e3
TOMOYAITO0726 <tomoyaito199507265724@gmail.com>
2023-02-09 12:44:57 +0000

    [宣言と代入]

---
+++ 変数と宣言/UseVariables.java
@@ -0,0 +1,11 @@
+public class UseVariables{
+  public static void main(String[] args){
+    String name= "佐藤";
+    int num=551;
+    double dec =67.8;
+    
+    System.out.println(name);
+    System.out.println(num);
+    System.out.println(dec);
+  }
+}
