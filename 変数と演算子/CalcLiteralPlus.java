f0c10bd61360da124ac7a51c3889c2ee6aa77b2f
TOMOYAITO0726 <tomoyaito199507265724@gmail.com>
2023-02-11 13:05:19 +0000

    [ループ復習]

--- 変数と演算子/CalcLiteralPlus.java
+++ 変数と演算子/CalcLiteralPlus.java
@@ -1,4 +1,4 @@
-public class CalcLiteraPlus {
+public class CalcLiteralPlus {
   public static void main(String[] args) {
     int ans = 35 + 446;
     System.out.println(ans);
---
+++ 変数と演算子/DivisionOperation1.java
@@ -0,0 +1,10 @@
+public class DivisionOperation1{
+  public static void main(String[] args){
+    int num1 = 6;
+    int num2 = 5;
+    
+    double ans = (double) num1 / num2;
+    
+    System.out.println(ans);
+  }
+}
