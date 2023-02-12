3d804a1c873f5cb56596ac0dee786fb93ede8045
TOMOYAITO0726 <tomoyaito199507265724@gmail.com>
2023-02-12 12:31:41 +0000

    [配列とループの応用]

--- 配列/ArrayLoop.java
+++ 配列/ArrayLoop.java
@@ -1,8 +1,8 @@
 public class ArrayLoop{
   public static void main(String[] args){
-    int[] nums ={8,9,17,26,43};
+    int[] nums ={8,9,17,26,43,45,49};
     
-    for(int i=0; i<=4; i++){
+    for(int i=0; i<=6; i++){
       System.out.println((i+1)+"個目:"+nums[i]);
     }
   }
