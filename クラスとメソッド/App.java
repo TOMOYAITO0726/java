b9261fcc2beaf8d672b8f8b40a38062463f305b5
TOMOYAITO0726 <tomoyaito199507265724@gmail.com>
2023-02-14 13:07:07 +0000

    [複数クラス]

---
+++ クラスとメソッド/App.java
@@ -0,0 +1,10 @@
+public class App{
+  public static void main(String[] args){
+    int answer = areoftrinles(6,5);
+    System.out.println(answer);
+  }
+  
+  public static int areoftrinles(int base, int height){
+    return base*height/2;
+  }
+}
