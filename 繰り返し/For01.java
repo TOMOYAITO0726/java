e5dc4f032da19a9bf42d23ba261facaece1d6ecf
TOMOYAITO0726 <tomoyaito199507265724@gmail.com>
2023-02-16 12:02:58 +0000

    [配列]

--- 繰り返し/For01.java
+++ 繰り返し/For01.java
@@ -1,6 +1,6 @@
 public class For01{
   public static void main(String[] args){
-    for (int number =1; number <=5; number++){
+    for (int number =1; number <=6; number++){
       System.out.println("For01 =" +number);
     }
   }
