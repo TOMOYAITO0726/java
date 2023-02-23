0a882f1bb4474080fe65e83b61cc2ab2dd83e5e5
TOMOYAITO0726 <tomoyaito199507265724@gmail.com>
2023-02-23 10:05:35 +0000

    [色々な型]

--- 型の基本/TypeSample.java
+++ 型の基本/TypeSample.java
@@ -3,5 +3,17 @@ public class TypeSample {
     //整数
     int sum = 5;
     System.out.println(sum);
+    
+    //浮動小数点数
+    double rate = 10.5;
+    System.out.println(rate);
+    
+    //真偽値
+    boolean isHoliday = true;
+    System.out.println(isHoliday);
+    
+    //文字列
+    String title ="いちばんやさしいjava";
+    System.out.println(title);
   }
 }
