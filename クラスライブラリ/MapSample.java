8c1b7fcc1b3c1bdf653661a99dbe2a49924601fc
TOMOYAITO0726 <tomoyaito199507265724@gmail.com>
2023-03-31 12:55:12 +0000

    [インタフェース]

---
+++ クラスライブラリ/MapSample.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapSample {
+  public static void main(String[] args) {
+    Map<String, Integer> fruitsMap = new HashMap<>();
+  
+    fruitsMap.put("りんご", 100);
+    fruitsMap.put("みかん", 150);
+    fruitsMap.put("ばなな", 250);
+    
+    System.out.println("りんご" + fruitsMap.get("りんご"));
+    System.out.println("みかん" + fruitsMap.get("みかん"));
+    System.out.println("ばなな" + fruitsMap.get("ばなな"));
+  }
+}
